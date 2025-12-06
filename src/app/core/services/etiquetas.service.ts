48176b8ed81e9ee5bf937d72260c0bd4345eea6b
Erick Pineda <epcloud2012@gmail.com>
2025-12-06 21:32:52 +0100

    Remove etiquetas.service.ts from core services
    Deleted the EtiquetasService, which extended EtiquetaService and re-exported AsociacionEtiquetaDTO. This may be part of a refactor or cleanup to remove unused or redundant services.

--- src/app/core/services/etiquetas.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@angular/core';
-import { EtiquetaService, AsociacionEtiquetaDTO } from './data/etiqueta.service';
-
-export { AsociacionEtiquetaDTO };
-
-@Injectable({ providedIn: 'root' })
-export class EtiquetasService extends EtiquetaService {}
